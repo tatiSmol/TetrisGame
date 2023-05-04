5fe9c19dcb13dc930740b8d2f95778473bb18437
tatiSmol <judeclapton@icloud.com>
2023-05-04 23:31:17 +0300

    Spawning new blocks.

--- src/main/java/GameArea.java
+++ src/main/java/GameArea.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class GameArea extends JPanel {
     private int gridRows, gridColumns, gridCellSize;
     private TetrisBlock block;
+    private Color[][] background;
 
 
     public GameArea(JPanel placeholder, int columns) {
@@ -16,12 +17,11 @@ public class GameArea extends JPanel {
         gridCellSize = this.getBounds().width / gridColumns;
         gridRows = this.getBounds().height / gridCellSize;
 
-        spawnBlock();
+        background = new Color[gridRows][gridColumns];
     }
 
     public void spawnBlock() {
-        block = new TetrisBlock(new int[][] {{1, 0}, {1, 0}, {1, 1}},
-                Color.BLUE);
+        block = new TetrisBlock(new int[][] {{1, 0}, {1, 0}, {1, 1}}, Color.BLUE);
         block.spawn(gridColumns);
     }
 
@@ -36,29 +36,65 @@ public class GameArea extends JPanel {
                 if (sh[row][col] == 1) {
                     int x = (block.getX() + col) * gridCellSize;
                     int y = (block.getY() + row) * gridCellSize;
-
-                    g.setColor(c);
-                    g.fillRect(x, y, gridCellSize, gridCellSize);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(x, y, gridCellSize, gridCellSize);
+                    drawGridSquare(g, c, x, y);
                 }
             }
         }
+    }
+
+    private void drawBackground(Graphics g) {
+        Color color;
+        for (int r = 0; r < gridRows; r++) {
+            for (int c = 0; c < gridColumns; c++) {
+                color = background[r][c];
+                if (color != null) {
+                    int x = c * gridCellSize;
+                    int y = r * gridCellSize;
+
+                    drawGridSquare(g, color, x, y);
+                }
+            }
+        }
+    }
+
+    private void moveBlockToBackground() {
+        int[][] shape = block.getShape();
+        int h = block.getHeight(), w = block.getWidth();
+        int xPos = block.getX(), yPos = block.getY();
+        Color color = block.getColor();
+
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++){
+                if (shape[r][c] == 1) {
+                    background[r + yPos][c + xPos] = color;
+                }
+            }
+        }
+    }
+
+    private void drawGridSquare(Graphics g, Color c, int x, int y) {
+        g.setColor(c);
+        g.fillRect(x, y, gridCellSize, gridCellSize);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, gridCellSize, gridCellSize);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawBlock(g);
+        drawBackground(g);
     }
 
-    public void moveBlockDown() {
+    public boolean moveBlockDown() {
         if (!checkBottom()) {
-            return;
+            moveBlockToBackground();
+            return false;
         }
 
         block.moveDown();
         repaint();
+        return true;
     }
 
     private boolean checkBottom() {
--- src/main/java/GameThread.java
+++ src/main/java/GameThread.java
@@ -2,21 +2,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GameThread extends Thread{
-    private GameArea ga;
+    private GameArea gameArea;
 
-    public GameThread(GameArea ga) {
-        this.ga = ga;
+    public GameThread(GameArea gameArea) {
+        this.gameArea = gameArea;
     }
 
     @Override
     public void run() {
         while (true) {
-            try {
-                ga.moveBlockDown();
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GameThread.class.getName())
-                        .log(Level.SEVERE, null, ex);
+            gameArea.spawnBlock();
+
+            while (gameArea.moveBlockDown()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, e);
+                }
             }
         }
     }
